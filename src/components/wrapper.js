16a1d97d21e7a37f553edea58b38610188ed1bdb
Chandler L <cbl0015@auburn.edu>
2020-10-22 01:22:51 -0400

    Creating Majority of Components Files

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,1 +1,12 @@
-// TBD
+import React from 'react';
+import "../styles/Wrapper.css";
+
+function Wrapper({ children }) {
+  return (
+      <div className="wrapper">
+        { children }
+      </div>
+  );
+}
+
+export default Wrapper;
